c4ad4e9da9fc29d151f1087aad57344aa8e2b2c2
nmusaelian-rally <nmusaelian@rallydev.com>
2016-07-04 10:10:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Sinatra app that searches commits by user/organization and pushed_at date
+=========================
+
+### configure
+
+create `configs` folder in the root directory, with a yaml file following this template:
+
+```
+user:		    your_github_user
+password: 	    your_pass
+base_url:           https://api.github.com
+```
+
+### requires:
+require 'sinatra'
+require 'shotgun'
+require 'github_inator' # source availabe from [github_inator repo](https://github.com/nmusaelian-rally/github_inator)
+require 'time'
+
+### run
+
+`shotgun app.rb`
+
+### License
+
+AppTemplate is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
+
+
+
---
+++ app.rb
@@ -0,0 +1,88 @@
+require 'sinatra'
+require 'shotgun'
+require 'github_inator'
+require 'time'
+
+
+REPO_COMMITS_ENDPOINT = "repos/<org_name>/<repo_name>/commits"
+SEARCH_REPOS_ENDPOINT = "search/repositories"
+INFLATED_COMMIT_ENDPOINT = "repos/<org_name>/<repo_name>/commits/<sha>"
+
+def get_all_results(connector, method, endpoint, options={}, data=nil, extra_headers=nil)
+  total_results = []
+  response = connector.make_request(method, endpoint,options,data,extra_headers)
+  total_results << response.body
+  while response.next != nil do
+    response = connector.make_request(:get, response.next)
+    total_results << response.body
+  end
+  total_results
+end
+
+def get_commits_data(owner, lookback)
+    config       = YAML.load_file('./configs/github.yml')
+    lookback = (Time.now - lookback.to_i * 3600).iso8601
+    @connector = GithubInator::GithubConnector.new(config)
+
+    repos_with_recent_commits = []
+    commits_data = []
+    criteria = "user:#{owner}+pushed:>#{lookback}"
+    options = {q: criteria}
+    results = get_all_results(@connector, :get, SEARCH_REPOS_ENDPOINT, options).flatten
+    total_count = results.first["total_count"]
+    if total_count
+      results.each do |page|
+        page["items"].each do |result|
+          repos_with_recent_commits << {'name' => result["name"], 'org' => result["owner"]["login"]}
+        end
+      end
+    end
+    if !repos_with_recent_commits.empty?
+      since = {since: lookback}
+      repos_with_recent_commits.each do |repo|
+        replacements = {'<org_name>' => repo['org'], '<repo_name>' => repo['name']}
+        repo_commits_endpoint = REPO_COMMITS_ENDPOINT.gsub(/<\w+>/) {|match| replacements.fetch(match,match)}
+        commit_results = get_all_results(@connector, :get, repo_commits_endpoint, since).flatten
+        commit_results.each do |result|
+          replacements = {'<org_name>' => repo['org'], '<repo_name>' => repo['name'], '<sha>' => result['sha']}
+          inflated_commit_endpoint = INFLATED_COMMIT_ENDPOINT.gsub(/<\w+>/) {|match| replacements.fetch(match,match)}
+          inflated_commits = get_all_results(@connector, :get, inflated_commit_endpoint).flatten
+          files = []
+          commit = {}
+          commit['repo'] = repo['name']
+          inflated_commits.each do |inflated_result|
+            if(!inflated_result['files'].empty?)
+              inflated_result['files'].each do |file|
+                files << file['filename']
+              end
+            end
+            commit['sha'] = inflated_result['sha']
+            commit['author'] = inflated_result['commit']['author']['name']
+            commit['date']   = inflated_result['commit']['author']['date']
+            commit['files'] = files
+            commit['message'] = inflated_result['commit']['message'].gsub!("\n", " ")
+            
+            commits_data << commit
+          end
+        end
+      end
+    end
+    return commits_data
+end
+
+get '/' do
+  "Hello"
+end
+
+get '/search' do
+  erb :search
+end
+
+post '/search' do
+  redirect "/results/#{params[:owner]}/#{params[:lookback]}"
+end
+
+get '/results/:owner/:lookback' do
+  commits_data = get_commits_data(params[:owner],params[:lookback])
+  erb :results, :locals => {:commits => commits_data, :owner => params[:owner], :lookback => params[:lookback]}
+end
